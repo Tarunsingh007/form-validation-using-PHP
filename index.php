1d9234793fac04dd18a77a69427e024c1b60d1dc
Tarun <34616098+Tarunsingh007@users.noreply.github.com>
2018-03-09 22:32:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$nameErr = "Name is required";
 	else {
     $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
       $nameErr = "Only letters and white space allowed"; 
     }
@@ -20,7 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$emailErr = "email is required";
 	else {
     $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format"; 
     }
@@ -30,7 +28,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$websiteErr = "website is required";
 	else {
     $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid
     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
       $websiteErr = "Invalid URL"; 
     }    
@@ -47,10 +44,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	else
 	$phone = test_input($_POST["phone"]);
 
-	if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
+	if (empty($_POST["sex"])) {
+    $sexErr = "sex is required";
   } else {
-    $gender = test_input($_POST["gender"]);
+    $sex = test_input($_POST["sex"]);
   }
 }
 function test_input($data)
@@ -69,8 +66,8 @@ E-mail: <input type="email" name="email"><?php echo $emailErr; ?><br/><br/>
 phone number: <input type="tel" name="phone"><?php echo $phoneErr; ?><br/><br/>
 website: <input type="text" name="website"><?php echo $websiteErr; ?><br/><br/>
 Comment: <textarea name="comment" rows="5" cols="40"></textarea><br/><br/>
-gender: <input type="radio" name="sex">Female
-<input type="radio" name="sex">Male<?php echo " *$sexErr"; ?><br/><br/>
+Sex: <input type="radio" name="sex" <?php if (isset($sex) && $sex=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="sex" <?php if (isset($sex) && $sex=="male") echo "checked";?> value="male">Male <?php echo $sexErr; ?> <br/><br/>
 <input type="submit">
 </form>
 <?php
